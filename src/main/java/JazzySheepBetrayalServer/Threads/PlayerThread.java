215a9ea42cbb74ad4d0699ba49354b84b88cef0e
Owen Sullivan <multiojuice@gmail.com>
2019-02-23 17:25:27 -0500

    Added protocols to player

--- src/main/java/JazzySheepBetrayalServer/Threads/PlayerThread.java
+++ src/main/java/JazzySheepBetrayalServer/Threads/PlayerThread.java
@@ -4,17 +4,32 @@ import JazzySheepBetrayalServer.DataStuctures.GameBoard;
 import JazzySheepBetrayalServer.DataStuctures.Player;
 import JazzySheepBetrayalServer.DataStuctures.Type;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class PlayerThread implements Runnable {
     private Socket socket;
     private GameBoard gameBoard;
     private Player player;
+    private BufferedReader in;
+    private PrintWriter out;
 
     public PlayerThread(Socket socket, GameBoard gameBoard, String id, Type type) {
         this.socket = socket;
         this.gameBoard = gameBoard;
         this.player = new Player(id, type);
+
+        try {
+            in = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(),
+                    true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void run() {
@@ -24,12 +39,18 @@ public class PlayerThread implements Runnable {
 
     }
 
-    private void readyProtocol() {
-
+    private void readyProtocol() throws IOException {
+        out.println(player.toString());
+        String readyString = in.readLine();
+        if (!readyString.equals("true")) System.out.println("ERROR: NOT READY");
+        out.println(gameBoard.initialStateString());
     }
 
-    private void playProtocol() {
-
+    private boolean playProtocol() throws IOException {
+        String movement = in.readLine();
+        String gameBoardString = gameBoard.movePlayer(player.getId(), movement);
+        if (gameBoardString.equals("false")) return false;
+        out.println(gameBoardString);
     }
 
     private void endProtocol() {
